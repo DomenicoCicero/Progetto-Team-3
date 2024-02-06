3c056b00f47353d796e417145ce67bad68f37949
marco9727 <marco.albarella97@gmail.com>
2024-02-06 18:27:41 +0100

    commit

--- pag.review.04.js
+++ pag.review.04.js
@@ -2,13 +2,11 @@ const stars = document.querySelectorAll(".stars i");
 console.log(stars);
 
 stars.forEach((star, index1) => {
-  star.addEventListener("click", (e) => {
+  star.addEventListener("click", () => {
     stars.forEach((star, index2) => {
       index1 >= index2
         ? star.classList.add("active")
         : star.classList.remove("active");
-      console.log(star);
-      console.log(index1);
     });
   });
 });

3ae79e5412ea1e3f31b636e1134c60b07a40ff2a
marco9727 <marco.albarella97@gmail.com>
2024-02-07 17:29:34 +0100

    commit

--- assets/js/pag.review.04.js
+++ assets/js/pag.review.04.js
@@ -1,6 +1,7 @@
+let valueStar = 0;
 const stars = document.querySelectorAll(".stars i");
 stars.forEach((star, i) => {
-  star.addEventListener("click", () => {
+  star.addEventListener("mouseover", () => {
     stars.forEach((star, j) => {
       if (i + 1 >= j + 1) {
         star.classList.add("active");
@@ -8,6 +9,23 @@ stars.forEach((star, i) => {
         star.classList.remove("active");
       }
     });
+    star.addEventListener("mouseout", () => {
+      stars.forEach((star) => {
+        star.classList.remove("active");
+      });
+    });
+  });
+
+  star.addEventListener("click", () => {
+    stars.forEach((star, j) => {
+      if (i + 1 >= j + 1) {
+        star.classList.add("selected1");
+      } else {
+        star.classList.remove("selected1");
+      }
+    });
+    const p = document.getElementsByClassName("valuestar")[0];
+    p.innerText = i + 1 + " of 10";
   });
 });
 
